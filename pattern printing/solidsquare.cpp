59f4ba3a99636f699fd10b2b56216d957e679516
Abhishek prajapati <abhishekp2412btechcse2026@kccitm.edu.in>
2023-05-25 18:30:03 +0530

    Update solidsquare.cpp

--- pattern printing/solidsquare.cpp
+++ pattern printing/solidsquare.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    // solid square OR solid rectangle
+    int row,colume;
+    cout<<"enter row and colume : ";
+    cin>>row>>colume;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<colume;j++){
+            cout<<" *";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
