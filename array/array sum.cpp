8fe414c09a90c95d8f931d9ef4c6683aa1d603d9
Abhishek prajapati <abhishekp2412btechcse2026@kccitm.edu.in>
2023-05-25 23:01:13 +0530

    Update array sum.cpp

--- array/array sum.cpp
+++ array/array sum.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    // sum of all element in an array
+    int size,sum=0;
+    cout<<"enter size of array : ";
+    cin>>size;
+    cout<<"enter the element : ";
+    int array[size];
+    for(int i=0;i<size;i++){
+        cin>>array[i];
+        sum=sum+array[i];
+    }
+    cout<<"sum of elements : "<<sum<<endl;
+    return 0;
+}
