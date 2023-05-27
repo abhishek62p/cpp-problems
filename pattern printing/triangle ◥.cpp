204fe2e193ec6ca0bcc528c1753fbee92dc8ae6c
Abhishek prajapati <abhishekp2412btechcse2026@kccitm.edu.in>
2023-05-27 12:59:39 +0530

    Create triangle ◥.cpp

---
+++ pattern printing/triangle ◥.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main (){
+    // right angle triangle ◥
+    int size;
+    cout<<"enetr the size : ";
+    cin>>size;
+    for(int i=size;i>0;i--){
+        for(int j=i;j<size;j++){
+                cout<<"  ";
+        }
+        for(int j=0;j<i;j++){
+                cout<<" *";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
